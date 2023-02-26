3d339e351d0ba6fa6d69a4ff20888d2c663dd08e
Mohammed Alaa <moalaacs@gmail.com>
2023-02-27 00:03:41 +0200

    Create a component with CLI

--- Lab1/my-app/src/app/app.module.ts
+++ Lab1/my-app/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TeacherComponent } from './teacher/teacher.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeacherComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, TeacherComponent]
 })
 export class AppModule { }
