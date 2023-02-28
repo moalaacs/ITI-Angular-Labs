62563a946caa6dab96f0856c9519f5c6a80781ed
Mohammed Alaa <moalaacs@gmail.com>
2023-02-28 02:37:11 +0200

    Delete operation

--- Lab2/my-app/src/app/students-list/students-list.component.ts
+++ Lab2/my-app/src/app/students-list/students-list.component.ts
@@ -10,10 +10,14 @@ export class StudentsListComponent {
   add(newStudent:Student){
     this.students.push(newStudent);
   }
+  delete(id:number){
+    console.log(id);
+    this.students=this.students.filter(std=>std.id !== id);
+  }
   students:Student[]=[
     new Student(1,"Ahmed",8),
     new Student(2,"Khaled",10),
-    new Student(2,"Gamal",9),
-    new Student(2,"Ali",8),
+    new Student(3,"Gamal",9),
+    new Student(4,"Ali",8),
   ];
 }
